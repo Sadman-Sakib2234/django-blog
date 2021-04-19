1abeeef79251a4846a7a072c7ea5260a6133de90
Sadman_Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-04-20 04:58:08 +0600

    Completed Project

---
+++ static/js/tinyInject.js
@@ -0,0 +1,29 @@
+var script= document.createElement('script');
+script.type='text/javascript';
+script.src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js";
+document.head.appendChild(script);
+
+script.onload=function(){
+    tinymce.init({
+      height:400,
+      selector: 'textarea',
+      plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+      toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+      toolbar_mode: 'floating',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Author name',
+        plugins: [
+      'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+      'table emoticons template paste help'
+    ],
+    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+      'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+      'forecolor backcolor emoticons | help',
+    menu: {
+      favs: {title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons'}
+    },
+    menubar: 'favs file edit view insert format tools table help',
+    content_css: 'css/content.css'
+    });
+}
